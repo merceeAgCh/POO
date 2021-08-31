97f297dc30b45df143c4f93c2918334586990637
Mercedes A <mercedes.aguilar95@hotmail.com>
2021-08-31 12:29:41 -0500

    Add files via upload
    Ejercicios introductorios C#
    Clase 1.

---
+++ Ejercicios/holaMundo_dif.cs
@@ -0,0 +1,21 @@
+using System;
+
+/*
+created by MerceeAC
+Segundo Programa en C#
+Clase 1
+Programación Orientada a Objetos.
+Agosto 2021
+*/
+public class hola{
+	public static void Main(){
+		System.Console.WriteLine("Hola, bienvenido a POO con aplicación en C#");
+
+		//Que diferencia hay entre esta impresión a pantalla
+
+		Console.WriteLine();
+
+		Console.WriteLine("Hola, bienvenido a POO con aplicación en C#");
+		//A esta impresión en pantalla
+	}	
+}
