8431a33ae48ef8edb010bb437726484d35385329
Mercedes A <mercedes.aguilar95@hotmail.com>
2021-09-08 18:16:51 -0500

    Add files via upload
    Diferencia entre Write y WriteLine

---
+++ Ejercicios/DiferenciaWrite_WriteLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DiferenciasWrite_WriteLine
+{
+    class WriteWriteLine
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe tu nombre y presiona enter para continuar: \n");
+            string name = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + name);
+            Console.ReadKey();
+        }
+    }
+}
