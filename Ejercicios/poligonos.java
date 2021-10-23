cf8b2f46426f2a76fbc024655cf8b9d49d873003
Mercedes Aguilar Chagoyán <mercedes.aguilar@mekitec.com>
2021-10-23 14:15:43 -0500

    Modificar el siguiente codigo para que haga lo deseado POO

---
+++ Ejercicios/poligonos.java
@@ -0,0 +1,24 @@
+/**
+
+Escribe los nombres y formulas del parimetro y area de los poligonos de 3,4,5 y 6 lados, entonces elabora un programa que estos calculos
+Lados   Nombre   Perimetro    Area
+3        -----    ------      ----
+4        -----    ------      ----
+5        -----    ------      ----
+6        -----    ------      ----
+*/
+
+using System;
+
+
+namespace SharpDev{
+	class array1{
+
+		int[] arr = new int[14]; //va de 0 a 15
+		//const int arr = 15; 
+
+		static void InitArr(){
+			/** Como hacer que lleve la secuencia a, b y c*/
+		}
+	}
+}
---
+++ Ejercicios/triangulocalculo.java
@@ -0,0 +1,59 @@
+/**Calculo del area de un triangulo Forma 1*/
+import java.util.*;
+
+public class poligonoE {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double b,h,l, lon;
+        System.out.println("Ingresa la base del poligono ");
+        b=sc.nextDouble();
+        System.out.println("Ingresa la altura del poligono ");
+        h=sc.nextDouble();
+        System.out.println("Ingresa los lados que tiene el poligono ");
+        l=sc.nextDouble();
+        double area;
+        area = b*h/2;
+        System.out.print("El area del poligono es: " + area);
+        double perimetro;
+        perimetro = l+lon;
+        System.out.println("El perimetro del poligono es: " + perimetro); 
+        //Determinar como sacar la longitud para realizar el calculo correspondiente del perimetro
+        //Responde las siguientes preguntas "Esto me sirve?, Debo modificar algo?, 
+        //Por que sirve y porque modificar o por que no."
+    }
+}
+
+/**Calculo del area de un triangulo Forma 2
+
+
+
+public class usartriangulo {
+    public static void main(String []arg){
+        triangulo a = new triangulo();
+        triangulo b = new triangulo(6 , 4);
+        triangulo c;
+        Scanner datin = new Scanner(System.in );
+        System.out.println("Ingrese el valor de la base: ");
+        a.setBase(datin.nextInt());
+        System.out.println("Ingresa el valor de la altura: ");
+        a.setAltura(datin.nextInt());
+        System.out.println("El triangulo mas grande es: ");
+        if (a.getArea() > b.getArea()){
+            System.out.println("El triangulo ingresado \n");
+            c = a;
+            System.out.println("Ingrese otro triangulo base: ");
+            a.setBase(datin.nextInt());
+            System.out.println("Altura ");
+            a.setBase(datin.nextInt());
+             if (a.getPerimetro()> c.getPerimetro()){
+                 System.out.println("El segundo triangulo tiene mas perimetro ");
+                 else
+                 System.out.println("El primer triangulo tiene mas perimetro");
+
+             }
+            else System.out.println("el triangulo mas grande es " + "Mas grande");
+        }
+
+    }
+}*/
