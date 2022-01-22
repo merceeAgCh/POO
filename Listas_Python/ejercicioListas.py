4d83591515a72cad815ccb410bea17de9a894373
Mercedes AC <breakh_070795@hotmail.com>
2022-01-22 14:20:02 -0600

    ToDo en archivo ejerciciosListas, llevar a cabo los metodos vistos

---
+++ Listas_Python/ejercicioListas.py
@@ -0,0 +1,16 @@
+#Lista de articulos de una tienda
+
+articulos = [12.5, "galleta", 15]
+articulos
+print(articulos)
+
+#ToDo
+#Agrega dos articulos a la lista utilizando el indice
+
+#Agrega el precio de los articulos
+
+#Utiliza el metodo append
+
+#imprime la lista
+
+#Elimina todos los datos de tipo string e imprime la lista en orden de mayor a menor
