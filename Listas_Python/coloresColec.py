f54d6f4436c582c15b1c7708dd4fc18870f4522c
Mercedes AC <breakh_070795@hotmail.com>
2022-01-23 12:40:34 -0600

    ejercicio 3 en clase, coleccion de colores

---
+++ Listas_Python/coloresColec.py
@@ -0,0 +1,11 @@
+#ejercicio en clase 3
+"""
+ToDo
+Mostrar menu
+Crear una coleccion de colores (3)
+Mostrar la coleccion
+Agregar color a la coleccion
+Editar un valor de la coleccion
+Eliminar dato
+Salir del programa.
+"""
