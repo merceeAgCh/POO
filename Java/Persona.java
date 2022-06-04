a24a1c748f0b2d902476f0fb5468a70af5fef045
jorge170 <75293169+jorge170@users.noreply.github.com>
2022-06-04 15:32:39 -0300

    Objeto agregado para mostrar dos resultados

--- Java/Persona.java
+++ Java/Persona.java
@@ -10,9 +10,12 @@ public class Persona{
 		//creamos el objeto y su instancia
 		Persona personaJorge = new Persona();
 		//referenciar instancia
-		int edadPersona = personaJorge.obtenerEdad(2022, 1995);
+		int edadPersona = personaJorge.obtenerEdad(1995,2022);
 		System.out.println("La edad de Jorge es: " + edadPersona);
-
+            //referenciar instancia dos
+            Persona personaJuan = new Persona();
+            int edadPersonaJuan = personaJuan.obtenerEdad(1985,2022);
+            System.out.println("La edad de la persona dos es: " + edadPersonaJuan);
 	}
 	public int obtenerEdad(int annioNacimiento, int annioActual){
 		edad = annioActual - annioNacimiento;
