7c11bdcc893544ef742d9bf9d83e6a9fa5a0baf4
Mercedes AC <breakh_070795@hotmail.com>
2022-06-04 14:03:19 -0500

    Clase 2 de Java. PrimeraC herencia

---
+++ Java/Auto.java
@@ -0,0 +1,20 @@
+/* ToDo Complementar las siguientes clases, 
+tomando en cuenta la herencia entre clases de java 
+*/
+
+public class Auto{
+	
+}
+
+class Vendedor{
+	
+}
+
+class Duenio{
+
+}
+
+class AutoLavado{
+
+}
+
