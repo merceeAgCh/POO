d80d2292f3f0992abde6ec78be6ea21b3d4479fb
jorge170 <75293169+jorge170@users.noreply.github.com>
2022-06-25 15:14:38 -0300

    Herencia y sobreescritura

---
+++ Persona.java
@@ -0,0 +1,24 @@
+public class Persona{
+// atributos: edad, sexo, estatura y nombre
+	private String sexo;
+	private int edad;
+	private String nombre;
+
+	public static void main(String... args){
+		// metodo principal....
+		System.out.println("Segunda clase de JAVA.");
+		//creamos el objeto y su instancia
+		Persona personaJorge = new Persona();
+		//referenciar instancia
+		int edadPersona = personaJorge.obtenerEdad(1995,2022);
+		System.out.println("La edad de Jorge es: " + edadPersona);
+            //referenciar instancia dos
+            Persona personaJuan = new Persona();
+            int edadPersonaJuan = personaJuan.obtenerEdad(1985,2022);
+            System.out.println("La edad de la persona dos es: " + edadPersonaJuan);
+	}
+	public int obtenerEdad(int annioNacimiento, int annioActual){
+		edad = annioActual - annioNacimiento;
+		return edad;
+	}
+}
